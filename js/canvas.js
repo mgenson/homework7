712be03c4534ecc86a161ba5dc412a77182eed55
mgenson <46972714+mgenson@users.noreply.github.com>
2020-03-22 15:59:14 -0400

    Add files via upload

---
+++ js/canvas.js
@@ -0,0 +1,20 @@
+// Variables!
+var color ="red";
+var radius = 15;
+var c = documnent.getElementById("canvas");
+//You will want to add more
+
+
+//Listeners!!
+var circle = c.getContext("2d");
+circle.beginPath();
+circle.arc(95, 50, 40, 0, 2 * Math.PI);
+circle.stroke();
+//Add a listener for loading the window
+//Add a listener for the mouse movement
+//Add a listener for the touch move
+//Add a listener for the keydown
+
+
+// Functions!
+// I would add a function for draw
