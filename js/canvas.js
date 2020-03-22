41b4a14877bac8092f879c4631441f1fac3e1fbd
mgenson <46972714+mgenson@users.noreply.github.com>
2020-03-22 16:10:44 -0400

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,13 @@
 // Variables!
 var color ="red";
 var radius = 15;
-var c = documnent.getElementById("canvas");
+var canvas = document.getElementById("canvas");
 //You will want to add more
 
 
 //Listeners!!
-var circle = c.getContext("2d");
+var circle = canvas.getContext("2d");
+circle.fillStyle = "FF000";
 circle.beginPath();
 circle.arc(95, 50, 40, 0, 2 * Math.PI);
 circle.stroke();
