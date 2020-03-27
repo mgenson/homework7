44ebb5f6ec3c1021c50c484f6dc8d8007ff2b64a
mgenson <46972714+mgenson@users.noreply.github.com>
2020-03-27 12:54:52 -0400

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -10,6 +10,7 @@ var y = 75;
 //You will want to add more
 
 //Listeners!!
+// Mouse move listener
 canvas.addEventListener('mousemove', function(e) {
   x = e.x;
   console.log(x);
@@ -17,21 +18,37 @@ canvas.addEventListener('mousemove', function(e) {
   console.log(y);
   draw(e.pageX - radius, e.pageY - radius);
 })
+// Change color with keydown
 draw();
+
 window.addEventListener('keydown', function(e){
   console.log(e.keyCode);
-  if (e.keyCode == 66);
+  if (e.keyCode == 66){
     ctx.fillStyle = "rgb(0, 0, 255)";
     console.log("changed to blue");
-  if (e.keyCode == 71);
+  }
+  if (e.keyCode == 71){
     ctx.fillStyle = "rgb(0, 255, 0)";
     console.log("chaged to green");
-  if(e.keyCode == 89);
-    ctx.fillStyle = "rgb(255, 255, 0)";
+  }
+
+  if(e.keyCode == 89){
     console.log("changed to yellow");
-  if(e.keyCode ==82);
-    ctx.fillStyle = "rgb(255, 0, 0)";
+    ctx.fillStyle = "rgb(255, 255, 0)";
+
+  }
+
+  if(e.keyCode == 82){
     console.log("changed to red");
+    ctx.fillStyle = "rgb(255, 0, 0)";
+
+  }
+
+  if (e.keyCode == 32){
+    ctx.clearRect(0, 0, canvas.height, canvas.width);
+    console.log("cleared canvas");
+  }
+
 })
 //Add a listener for loading the window
 //Add a listener for the mouse movement
